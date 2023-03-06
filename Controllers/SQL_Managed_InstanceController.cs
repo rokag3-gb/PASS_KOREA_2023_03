8407743f726da98fa14c033cca554e3d58639fc4
Jungwoo Kim <rokag3@hotmail.com>
2023-03-06 15:42:09 +0900

    Controller 를 SQL_DatabaseController 와 SQL_Managed_InstanceController 로 구분지어 수정함.

--- Controllers/SQL_Managed_InstanceController.cs
+++ Controllers/SQL_Managed_InstanceController.cs
@@ -2,14 +2,14 @@ namespace WebAPI
 {
     [ApiController]
     [Route("[controller]")]
-    public class Public_endpointController : ControllerBase
+    public class SQL_Managed_InstanceController : ControllerBase
     {
         private readonly DapperContext _context;
-        private readonly ILogger<Public_endpointController> _logger;
+        private readonly ILogger<SQL_Managed_InstanceController> _logger;
 
-        public Public_endpointController(
+        public SQL_Managed_InstanceController(
             DapperContext context
-            , ILogger<Public_endpointController> logger)
+            , ILogger<SQL_Managed_InstanceController> logger)
         {
             _context = context;
             _logger = logger;
@@ -75,13 +75,13 @@ namespace WebAPI
             }
         }
 
-        [Route("/Deal/sqldb_public_primary/")]
+        [Route("/Deal/sqlmi_local_primary/")]
         [HttpGet]
-        public async Task<IEnumerable<DealModel>> GetDeal_sqldb_public_primary(
+        public async Task<IEnumerable<DealModel>> GetDeal_sqlmi_local_primary(
             string DealDateStart = null
             , string DealDateEnd = null)
         {
-            string conn_str = Secret.conn_str_sqldb_public_primary;
+            string conn_str = Secret.conn_str_sqlmi_local_primary;
             //Console.WriteLine($"conn.ConnectionString = {conn_str.Split(';')?.GetValue(0)?.ToString()}");
 
             try
@@ -90,7 +90,7 @@ namespace WebAPI
                 {
                     if (conn.State != ConnectionState.Open) conn.Open();
 
-                    var deals = await conn.QueryAsync<DealModel>(Query.str_query.Replace("db1.dbo.Deal", "dbo.Deal")
+                    var deals = await conn.QueryAsync<DealModel>(Query.str_query
                         , new { DealDateStart = DealDateStart, DealDateEnd = DealDateEnd }
                         );
 
@@ -105,13 +105,13 @@ namespace WebAPI
             }
         }
 
-        [Route("/Deal/sqldb_public_secondary/")]
+        [Route("/Deal/sqlmi_local_secondary/")]
         [HttpGet]
-        public async Task<IEnumerable<DealModel>> GetDeal_sqldb_public_secondary(
+        public async Task<IEnumerable<DealModel>> GetDeal_sqlmi_local_secondary(
             string DealDateStart = null
             , string DealDateEnd = null)
         {
-            string conn_str = Secret.conn_str_sqldb_public_secondary;
+            string conn_str = Secret.conn_str_sqlmi_local_secondary;
             //Console.WriteLine($"conn.ConnectionString = {conn_str.Split(';')?.GetValue(0)?.ToString()}");
 
             try
@@ -120,7 +120,67 @@ namespace WebAPI
                 {
                     if (conn.State != ConnectionState.Open) conn.Open();
 
-                    var deals = await conn.QueryAsync<DealModel>(Query.str_query.Replace("db1.dbo.Deal", "dbo.Deal")
+                    var deals = await conn.QueryAsync<DealModel>(Query.str_query
+                        , new { DealDateStart = DealDateStart, DealDateEnd = DealDateEnd }
+                        );
+
+                    if (conn.State == ConnectionState.Open) conn.Close();
+
+                    return deals;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [Route("/Deal/sqlmi_fog_readwrite/")]
+        [HttpGet]
+        public async Task<IEnumerable<DealModel>> GetDeal_sqlmi_fog_readwrite(
+            string DealDateStart = null
+            , string DealDateEnd = null)
+        {
+            string conn_str = Secret.conn_str_sqlmi_fog_readwrite_listener;
+            //Console.WriteLine($"conn.ConnectionString = {conn_str.Split(';')?.GetValue(0)?.ToString()}");
+
+            try
+            {
+                using (IDbConnection conn = _context.CreateConnection(conn_str))
+                {
+                    if (conn.State != ConnectionState.Open) conn.Open();
+
+                    var deals = await conn.QueryAsync<DealModel>(Query.str_query
+                        , new { DealDateStart = DealDateStart, DealDateEnd = DealDateEnd }
+                        );
+
+                    if (conn.State == ConnectionState.Open) conn.Close();
+
+                    return deals;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [Route("/Deal/sqlmi_fog_readonly/")]
+        [HttpGet]
+        public async Task<IEnumerable<DealModel>> GetDeal_sqlmi_fog_readonly(
+            string DealDateStart = null
+            , string DealDateEnd = null)
+        {
+            string conn_str = Secret.conn_str_sqlmi_fog_readonly_listener;
+            //Console.WriteLine($"conn.ConnectionString = {conn_str.Split(';')?.GetValue(0)?.ToString()}");
+
+            try
+            {
+                using (IDbConnection conn = _context.CreateConnection(conn_str))
+                {
+                    if (conn.State != ConnectionState.Open) conn.Open();
+
+                    var deals = await conn.QueryAsync<DealModel>(Query.str_query
                         , new { DealDateStart = DealDateStart, DealDateEnd = DealDateEnd }
                         );
 
@@ -209,11 +269,11 @@ namespace WebAPI
             }
         }
 
-        [Route("/Deal/sqldb_public_primary/")]
+        [Route("/Deal/sqlmi_local_primary/")]
         [HttpPost]
-        public async Task<ActionResult> Insert_sqldb_public_primary(DealModel deal)
+        public async Task<ActionResult> Insert_sqlmi_local_primary(DealModel deal)
         {
-            string conn_str = Secret.conn_str_sqldb_public_primary;
+            string conn_str = Secret.conn_str_sqlmi_local_primary;
             //Console.WriteLine($"conn.ConnectionString = {conn_str.Split(';')?.GetValue(0)?.ToString()}");
 
             try
@@ -246,11 +306,85 @@ namespace WebAPI
             }
         }
 
-        [Route("/Deal/sqldb_public_secondary/")]
+        [Route("/Deal/sqlmi_local_secondary/")]
         [HttpPost]
-        public async Task<ActionResult> Insert_sqldb_public_secondary(DealModel deal)
+        public async Task<ActionResult> Insert_sqlmi_local_secondary(DealModel deal)
         {
-            string conn_str = Secret.conn_str_sqldb_public_secondary;
+            string conn_str = Secret.conn_str_sqlmi_local_secondary;
+            //Console.WriteLine($"conn.ConnectionString = {conn_str.Split(';')?.GetValue(0)?.ToString()}");
+
+            try
+            {
+                var param = new DynamicParameters();
+                param.Add("DealDate", deal.DealDate);
+                param.Add("Item", deal.Item.Trim());
+                param.Add("UserId", deal.UserId?.ToString());
+                param.Add("Amount", deal.Amount);
+                param.Add("Tax", deal.Tax);
+                param.Add("Note", deal.Note);
+
+                int effected_row = 0;
+
+                using (IDbConnection conn = _context.CreateConnection(conn_str))
+                {
+                    if (conn.State != ConnectionState.Open) conn.Open();
+
+                    effected_row = conn.Execute(Query.str_command, param);
+
+                    if (conn.State == ConnectionState.Open) conn.Close();
+                }
+
+                return StatusCode(200, $"effected_row = {effected_row}");
+                //return NoContent(); // 204
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [Route("/Deal/sqlmi_fog_readwrite/")]
+        [HttpPost]
+        public async Task<ActionResult> Insert_sqlmi_fog_readwrite(DealModel deal)
+        {
+            string conn_str = Secret.conn_str_sqlmi_fog_readwrite_listener;
+            //Console.WriteLine($"conn.ConnectionString = {conn_str.Split(';')?.GetValue(0)?.ToString()}");
+
+            try
+            {
+                var param = new DynamicParameters();
+                param.Add("DealDate", deal.DealDate);
+                param.Add("Item", deal.Item.Trim());
+                param.Add("UserId", deal.UserId?.ToString());
+                param.Add("Amount", deal.Amount);
+                param.Add("Tax", deal.Tax);
+                param.Add("Note", deal.Note);
+
+                int effected_row = 0;
+
+                using (IDbConnection conn = _context.CreateConnection(conn_str))
+                {
+                    if (conn.State != ConnectionState.Open) conn.Open();
+
+                    effected_row = conn.Execute(Query.str_command, param);
+
+                    if (conn.State == ConnectionState.Open) conn.Close();
+                }
+
+                return StatusCode(200, $"effected_row = {effected_row}");
+                //return NoContent(); // 204
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [Route("/Deal/sqlmi_fog_readonly/")]
+        [HttpPost]
+        public async Task<ActionResult> Insert_sqlmi_fog_readonly(DealModel deal)
+        {
+            string conn_str = Secret.conn_str_sqlmi_fog_readonly_listener;
             //Console.WriteLine($"conn.ConnectionString = {conn_str.Split(';')?.GetValue(0)?.ToString()}");
 
             try
